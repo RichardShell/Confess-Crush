336887cdf15fb260bca00f3fafaa9498c5ba3021
RichardShell <91361443+RichardShell@users.noreply.github.com>
2021-10-05 19:56:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Quốc Việt ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -14,7 +14,7 @@ const CONFIG = {
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 9h, mình alo nha  .',
+    messDesc: 'Tối nay 9h, mình call nha  .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/duclovecau/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
